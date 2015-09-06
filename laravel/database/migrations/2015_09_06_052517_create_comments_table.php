3172e475403702a1d2ef99bff3186bd9d7b7b894
phper <inaka-phper@users.noreply.github.com>
2015-09-06 14:35:03 +0900

    [add]postsとcommentsテーブルを追加
    編集のサンプルとして
    記事テーブルと、記事に紐づくコメントテーブルを追加

---
+++ laravel/database/migrations/2015_09_06_052517_create_comments_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('post_id');     // コメント記事ID
+            $table->integer('user_id');     // 書き込みユーザー
+            $table->text('content');        // コメント
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('comments');
+    }
+}
